47b4b08f27d03a6d9068e362b4bf0164afe9551e
BantaKondou <rei-016@hotmail.com>
2019-06-13 13:17:20 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 近藤万太 on 2019/06/13.
+//  Copyright © 2019 banta.kondou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
